e636c988fd05630a00c0b66e704a0a14474fe9a0
Piotr <piotrwydmuch95@gmail.com>
2020-07-05 11:16:56 +0200

    Testing API connection

--- App.tsx
+++ App.tsx
@@ -1,14 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    getComics();
+  }, [query]);
+
+  const getComics = async () => {
+    const response = await fetch(`https://xkcd.com/info.0.json`);
+    const data = await response.json();
+    console.log(data);
+  };
 
   if (!isLoadingComplete) {
     return null;
