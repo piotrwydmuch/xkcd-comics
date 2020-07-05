0cdc5ada3758416838d0e0b10b3a5f292ae99bd6
Piotr <piotrwydmuch95@gmail.com>
2020-07-05 15:36:02 +0200

    All comics display works. Next step: styling

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
+import {
+  StatusBar,
+  setStatusBarNetworkActivityIndicatorVisible,
+} from "expo-status-bar";
+import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
@@ -9,28 +12,6 @@ import Navigation from "./navigation";
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
-  const [query, setQuery] = useState("");
-
-  useEffect(() => {
-    getLastComics();
-  }, [query]);
-
-  const getLastComics = async () => {
-    //First we need to know, what is the last comic ID
-    const response = await fetch(`https://xkcd.com/info.0.json`);
-    const data = await response.json();
-    const lastComics = data.num;
-    const getLastEight = lastComics - 8;
-
-    //Now we are getting last 8 comics
-    let i: number = 0;
-    for (i = lastComics; i > getLastEight; i--) {
-      const response = await fetch(`https://xkcd.com/${i}/info.0.json`);
-      const data = await response.json();
-      console.log(data);
-    }
-  };
 
   if (!isLoadingComplete) {
     return null;
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,55 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Image } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+//import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
 
 export default function TabOneScreen() {
+  const [comics, setComics] = useState([{}]);
+
+  useEffect(() => {
+    getLastComics();
+  }, [setComics]);
+
+  const getLastComics = async () => {
+    //First we need to know, what is the last comic ID
+    const response = await fetch(`https://xkcd.com/info.0.json`);
+    const data = await response.json();
+    const lastComics = data.num;
+    const howManyComics = 8; //You can choose number of comics
+    const comicsNumber = lastComics - howManyComics;
+
+    //Now we are getting last x comics
+    let latestComics: any = [];
+    let i: number = 0;
+    for (i = lastComics; i > comicsNumber; i--) {
+      const response = await fetch(`https://xkcd.com/${i}/info.0.json`);
+      const data = await response.json();
+      console.log(data);
+      latestComics.push(data);
+    }
+    setComics(latestComics);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.title}>Last Comics</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      {comics.map((comic) => (
+        <View key={comic.num} style={styles.comicContainer}>
+          <Text>{comic.num}</Text>
+          <Image
+            style={styles.comicImage}
+            source={{
+              uri: comic.img,
+            }}
+          />
+        </View>
+      ))}
     </View>
   );
 }
@@ -17,16 +57,23 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  comicImage: {
+    height: 300,
+    width: "100%",
+  },
+  comicContainer: {
+    width: "100%",
   },
 });
