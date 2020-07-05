94acfc25f335a4d00de5c995b9caf60f7f814e1c
Piotr <piotrwydmuch95@gmail.com>
2020-07-05 13:21:13 +0200

    API calls last 8 comics

--- App.tsx
+++ App.tsx
@@ -13,13 +13,23 @@ export default function App() {
   const [query, setQuery] = useState("");
 
   useEffect(() => {
-    getComics();
+    getLastComics();
   }, [query]);
 
-  const getComics = async () => {
+  const getLastComics = async () => {
+    //First we need to know, what is the last comic ID
     const response = await fetch(`https://xkcd.com/info.0.json`);
     const data = await response.json();
-    console.log(data);
+    const lastComics = data.num;
+    const getLastEight = lastComics - 8;
+
+    //Now we are getting last 8 comics
+    let i: number = 0;
+    for (i = lastComics; i > getLastEight; i--) {
+      const response = await fetch(`https://xkcd.com/${i}/info.0.json`);
+      const data = await response.json();
+      console.log(data);
+    }
   };
 
   if (!isLoadingComplete) {
